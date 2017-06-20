5b959896235123d549b6b9ad4b7b46d53fd2e5fd
zeven <fangfan1997@126.com>
2017-06-20 11:50:31 +0800

    HomePage

---
+++ src/components/Swiper.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  TouchableHighlight
+} from 'react-native';
+import Swiper from 'react-native-swiper';
+
+export default class MySwiper extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let {data, onItemPress} = this.props;
+    let slides = [];
+    // console.warn(JSON.stringify(topArticles));
+    if (data) {
+      data.map((v, k) => {
+        slides.push(
+          <TouchableHighlight key={k} style={styles.slide} onPress={() => onItemPress(v, k)}>
+            <Image source={{uri:v.image}} style={styles.slide} resizeMode="cover">
+              <View style={styles.shadow}/>
+              <Text style={styles.title}>{v.title}</Text>
+            </Image>
+          </TouchableHighlight>
+        )
+      });
+      return(
+        <Swiper style={styles.wrapper}
+                dot={<View style={{backgroundColor:'rgba(150,150,150,.5)', width: 8, height: 8,borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3}} />}
+                activeDot={<View style={{backgroundColor: '#fff', width: 8, height: 8, borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3}} />}
+                paginationStyle={{bottom:5}}
+                height={240}
+                autoplay={true}
+                loop={false}
+                showsPagination={true}
+                autoplayTimeout={5}>
+          {slides}
+        </Swiper>
+      )
+    }
+    return null;
+  }
+
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+  },
+  shadow:{
+    backgroundColor:'#333',opacity:0.3,top:170,height:70
+  },
+  title:{
+    top:100,
+    marginLeft:20,
+    marginRight:20,
+    color:'white',
+    fontSize:20
+  },
+  slide: {
+    flex:1,
+    backgroundColor:'#343434'
+  }
+});
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {View, Text, Thumbnail} from 'native-base';
+import {View, Text, Thumbnail, Container, Content, Body, List, ListItem} from 'native-base';
+import Swiper from '../components/Swiper';
+import {connect} from 'dva/mobile';
 
 class HomePage extends React.Component {
 
@@ -9,21 +11,62 @@ class HomePage extends React.Component {
     }
   };
 
+  constructor(props){
+    super(props);
+    props.dispatch({
+      type: 'zhihu/getLatest'
+    })
+  }
+
   render() {
+    let {zhihu} = this.props;
     return (
-      <View style={styles.container}>
-        <Text style={{marginTop: 20}}>Click on the upper left corner to navigate.</Text>
-      </View>
+      <Container>
+        <Content>
+          <Swiper data={zhihu.topNews} onItemPress={alert}/>
+          <List>
+            {zhihu.dates.map(d=>(
+              zhihu.list[d] && zhihu.list[d].map(v=>(
+                <ListItem key={v.id}>
+                  <Body><Text>{v.title}</Text></Body>
+                  <Thumbnail square size={100} source={{uri: v.images[0]}} />
+                </ListItem>
+              ))
+            ))}
+          </List>
+        </Content>
+      </Container>
     );
   }
 }
 
 const styles = {
-  container: {
-    flex: 1,
+  wrapper: {
+    height: 200,
+  },
+  slide1: {
+    height: 200,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
   }
 };
 
-export default HomePage;
+export default connect(({zhihu})=>({zhihu}))(HomePage);
