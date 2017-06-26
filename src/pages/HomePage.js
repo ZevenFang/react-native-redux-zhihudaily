5181683950221ecb388c468a875b3b60236601cb
zeven <fangfan1997@126.com>
2017-06-26 15:21:50 +0800

    update title when scrolling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {RefreshControl} from 'react-native';
-import {View, Text, Thumbnail, Container, Body, List, ListItem, Separator, Icon} from 'native-base';
+import {View, Text, Thumbnail, Container, Body, List, ListItem, Separator, Icon, Grid} from 'native-base';
 import moment from 'moment';
 import Swiper from '../components/Swiper';
 import Loading from '../components/Loading';
@@ -12,13 +12,22 @@ let t = themes['light'];
 
 class HomePage extends React.Component {
 
+  pos = {};
+
   static route = {
     navigationBar: {
-      title: '首页',
+      title({title}){
+        return title||'首页';
+      },
       renderRight: (route, prop)=>(
-        <Touch style={styles.navBar}>
-          <Icon style={styles.navIcon} name="notifications" ios="md-notifications"/>
-        </Touch>
+        <Grid style={styles.navBar}>
+          <Touch style={styles.navBtn}>
+            <Icon style={styles.navIcon} name="notifications" ios="md-notifications"/>
+          </Touch>
+          <Touch style={styles.navBtn}>
+            <Icon style={styles.navIcon} name="more" ios="md-more"/>
+          </Touch>
+        </Grid>
       )
     }
   };
@@ -40,11 +49,29 @@ class HomePage extends React.Component {
     })
   };
 
-  _renderRow = (row) =>{
+  _onItemLayout = (e, date) => {
+    this.pos[date] = e.nativeEvent.layout.y;
+  };
+
+  _onScroll = (e) => {
+    let y = e.nativeEvent.contentOffset.y;
+    let dates = Object.keys(this.pos);
+    let title = '首页';
+    for (let i=0; i<dates.length; i++) {
+      let date = dates[i];
+      if (y >= this.pos[date])
+        title = date;
+      else break;
+    }
+    if (this.props.route.params.title !== title)
+      this.props.navigator.updateCurrentRouteParams({title})
+  };
+
+  _renderRow = row =>{
     return (
       row.date?
-        <Separator style={{height: 50, backgroundColor: t.background, marginBottom: -6}}>
-          <Text style={{color: 'grey', fontSize: 15}}>{row.date}</Text>
+        <Separator style={styles.separator} onLayout={e=>this._onItemLayout(e, row.date)}>
+          <Text style={styles.separatorText}>{row.date}</Text>
         </Separator>:
         <ListItem style={styles.listItem} onPress={alert}>
           <Body><Text>{row.title}</Text></Body>
@@ -77,6 +104,7 @@ class HomePage extends React.Component {
           <List style={{backgroundColor: t.listBg}}
                 dataArray={data} renderRow={this._renderRow}
                 removeClippedSubviews={false}
+                onScroll={this._onScroll}
                 onEndReachedThreshold={1}
                 onEndReached={!isEmpty&&this._onEndReached}
                 renderHeader={()=>(<Swiper data={zhihu.topNews} onItemPress={alert}/>)}
@@ -90,12 +118,15 @@ class HomePage extends React.Component {
 let styles = {
   navBar: {
     flex: 1,
-    padding: 10,
     justifyContent: 'center',
     alignItems: 'center'
   },
+  navBtn: {
+    padding: 15
+  },
   navIcon: {
-    color: 'white'
+    color: 'white',
+    fontSize: 25
   },
   listItem: {
     marginLeft: 0,
@@ -106,6 +137,16 @@ let styles = {
     borderTopWidth: 6,
     borderRightWidth: 8,
     borderBottomWidth: 2
+  },
+  separator: {
+    height: 50,
+    backgroundColor:
+    t.background,
+    marginBottom: -6
+  },
+  separatorText: {
+    color: 'grey',
+    fontSize: 14
   }
 };
 
